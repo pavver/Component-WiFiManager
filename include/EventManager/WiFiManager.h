f99f95e0c4b3494b783c65b8bd3893b1659c1c0a
pavver <pavvers1@gmail.com>
2024-10-11 01:32:29 +0300

    refactor & add comments

--- include/EventManager/WiFiManager.h
+++ include/EventManager/WiFiManager.h
@@ -1,25 +1,34 @@
 #pragma once
 
-// ╔════════════════════════════════════════════╗
-// ║                    WiFi                    ║
-// ╚════════════════════════════════════════════╝
-
-/// @brief EventType WiFi
+/**
+ * @brief EventType WiFi
+ */
 #define EventTypeWiFI (uint8_t)1
 
 // ----------------------------------------------
 
-/// @brief EventSubtype Команда підєднатись до WiFi
+/** 
+ * @brief EventSubtype request event to connect to a WiFi network
+ */
 #define EventSubtypeWiFI_Connect (uint8_t)1
 
-/// @brief EventSubtype Команда дізнатись список доступних WiFi мереж
+/**
+ * @brief EventSubtype request event to connect to a WiFi network
+ */
 #define EventSubtypeWiFI_GetAPs (uint8_t)2
 
-/// @brief EventSubtype Команда дізнатись статус роботи WiFi
+/**
+ * @brief EventSubtype request event to get WiFi status
+ */
 #define EventSubtypeWiFI_Status (uint8_t)3
 
-/// @brief EventSubtype Команда змінити налаштування точки доступу WiFi
+/**
+ * @brief EventSubtype request event to set WiFi AP settings
+ * 
+ */
 #define EventSubtypeWiFI_ApSettings (uint8_t)4
 
-/// @brief EventSubtype Команда вимкнути точку доступу WiFi
+/**
+ * @brief EventSubtype request event to turn off WiFi AP
+ */
 #define EventSubtypeWiFI_ApOff (uint8_t)5
--- include/WiFiManager/HttpEndpoints.h
+++ include/WiFiManager/HttpEndpoints.h
@@ -10,31 +10,51 @@
 // ║                    WiFi                    ║
 // ╚════════════════════════════════════════════╝
 
-/// @brief Список WiFi мереж
+/**
+ * @brief Send a request event to get a list of available WiFi networks
+ * @param req HTTP request
+ * @returns ESP_OK if successful
+ */
 static esp_err_t post_api_aps_handler(httpd_req_t *req)
 {
   return post_handler(req, EventTypeWiFI, EventSubtypeWiFI_GetAPs, 0, true);
 }
 
-/// @brief Підєднатись до WiFi мережі
+/**
+ * @brief Send a request event to connect to a WiFi network
+ * @param req HTTP request
+ * @returns ESP_OK if successful
+ */
 static esp_err_t post_api_connect_handler(httpd_req_t *req)
 {
   return post_handler(req, EventTypeWiFI, EventSubtypeWiFI_Connect, 128, true);
 }
 
-/// @brief Змінити налаштування WiFi точки доступу
+/**
+ * @brief Send a request event to set WiFi AP settings
+ * @param req HTTP request
+ * @returns ESP_OK if successful
+ */
 static esp_err_t post_api_apsettings_handler(httpd_req_t *req)
 {
   return post_handler(req, EventTypeWiFI, EventSubtypeWiFI_ApSettings, 128, true);
 }
 
-/// @brief Вимкнути WiFi точку доступу
+/**
+ * @brief Send a request event to turn off WiFi AP
+ * @param req HTTP request
+ * @returns ESP_OK if successful
+ */
 static esp_err_t post_api_apoff_handler(httpd_req_t *req)
 {
   return post_handler(req, EventTypeWiFI, EventSubtypeWiFI_ApOff);
 }
 
-/// @brief Поточний режим роботи
+/**
+ * @brief Send a request event to get WiFi status
+ * @param req HTTP request
+ * @returns ESP_OK if successful
+ */
 static esp_err_t post_api_status_handler(httpd_req_t *req)
 {
   return post_handler(req, EventTypeWiFI, EventSubtypeWiFI_Status, 0, true);
@@ -44,13 +64,18 @@ static esp_err_t post_api_status_handler(httpd_req_t *req)
 // ║                    HTTP                    ║
 // ╚════════════════════════════════════════════╝
 
+/**
+ * @brief Processing HTTP requests to work with WiFi
+ * @param req HTTP request
+ * @returns ESP_OK if successful
+ */
 static esp_err_t post_wifi_handler(httpd_req_t *req)
 {
   httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
   httpd_resp_set_type(req, "application/json");
 
   esp_err_t err = ESP_FAIL;
-  
+
   API_METHOD("status", &post_api_status_handler);
   API_METHOD("aps", &post_api_aps_handler);
   API_METHOD("connect", &post_api_connect_handler);
@@ -65,15 +90,18 @@ static esp_err_t post_wifi_handler(httpd_req_t *req)
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 
-const static char *apiwifi = "/api/wifi/*";
-
 static const httpd_uri_t http_server_post_wifi_request = {
-    .uri = apiwifi,
+    .uri = "/api/wifi/*",
     .method = HTTP_POST,
     .handler = post_wifi_handler};
 
 #pragma GCC diagnostic pop
 
+/**
+ * @brief Register HTTP endpoint work with WiFi
+ * @param handle HTTP server handle
+ * @returns ESP_OK if successful
+ */
 static esp_err_t register_wifi_command_handler(httpd_handle_t handle)
 {
   return httpd_register_uri_handler(handle, &http_server_post_wifi_request);
